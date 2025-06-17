8cc4dc611b473d7523b0a2cbea73d791c70a1ff1
labibj <ljamal2011@gmail.com>
2025-06-17 05:33:59 +0500

    build error fixes

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,29 +1,28 @@
 import { MongoClient } from "mongodb";
 
-if (!process.env.MONGODB_URI) {
-  throw new Error("Please add your MONGODB_URI to .env.local");
-}
-
-const uri: string = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI!;
 const options = {};
+
+// Extend NodeJS global type so we don't use `any`
+declare global {
+  // eslint-disable-next-line no-var
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
 
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
-declare global {
-  // allow global variable reuse in Next.js hot-reload
-  var _mongoClientPromise: Promise<MongoClient> | undefined;
+if (!process.env.MONGODB_URI) {
+  throw new Error("Please add your MONGODB_URI to .env.local");
 }
 
 if (process.env.NODE_ENV === "development") {
-  // In dev, reuse a global variable so we don't create new clients on hot reload
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
-  // In production, create a new client per request
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
